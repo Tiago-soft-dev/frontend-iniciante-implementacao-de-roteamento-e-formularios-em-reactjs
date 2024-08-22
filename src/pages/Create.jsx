b74d5ae47d82d6c616363c34711da96964bed0b1
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-22 12:45:32 -0300

    Criação do Form

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,32 @@
 export default function Create(){
     return (
     
-    <>
-    create
-    
-    </>
+   <div>
+    <h1>Criar Devmon</h1>
+    <form>
+        <div>
+            <label htmlFor="nome">Nome:</label><br></br>
+            <input type="text" id="nome" name="nome" placeholder="insira o nome"/>
+        </div><br></br>
+
+        <div>
+            <label htmlFor="imagem">Imagem (URL):</label><br></br>
+            <input type="text" id="imagem" name="imagem" placeholder="insira a URL da imagem"/>
+        </div><br></br>
+
+        <div>
+            <label htmlFor="evoluiPara">Evolui Para (opcional):</label><br></br>
+            <input type="text" id="evoluiPara" name="evoluiPara" placeholder="insira o nome da evolução"/>
+        </div><br></br>
+
+        <div>
+            <button type="submit">Criar</button>
+            
+            <button type="reset">Reiniciar</button>
+                       
+        </div>
+    </form>
+   </div>
     
 )
 }
